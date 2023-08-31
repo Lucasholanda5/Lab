eacf3878009e4e1eef14f0c0919b4632ab67dc68
lucasholanda5 <lucasholanda5@gmail.com>
2023-08-31 12:26:09 -0300

    teste3

---
+++ lista_1/questao_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float input;
+
+    printf("Digite um número: ");
+    scanf("%f", &input);
+
+    printf("Número com uma casa decimal: %.1f\n", input);
+
+    return 0;
+	
+}
