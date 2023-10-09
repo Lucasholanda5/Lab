79e736ef7df3445712dfca072c656c991476d301
Lucasholanda5 <143411386+Lucasholanda5@users.noreply.github.com>
2023-10-09 10:12:23 -0300

    Update questão_6.c

--- lista_3/questão_6.c
+++ lista_3/questão_6.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <math.h>
 
 int main(void) {
-    srand(time(NULL));
+  
+  int str[5];
 
-    int str[3];
-    int geo = 1, ari = 0, i;
+  for (int i = 0; i < 5; i++){
+    printf("Informe um valor para o vetor: \n");
+    scanf("%d", &str[i]);
 
-    for (i = 0; i < 3; i++) {
-        str[i] = rand() % 20;
-
-        ari += str[i];
-        geo *= str[i];
-    }
-
-    printf("Os números gerados são: %d, %d, %d\n", str[0], str[1], str[2]);
-    printf("A média aritmética é: %.1f\n", (float)ari / (i + 1));
-    printf("A média geométrica é: %.1f\n", pow((double)geo, 1.0 / (i + 1)));
-
-    return 0;
+  }
+  printf("============\n");
+  
+  for (int i = 4; i >= 0; i--){
+    printf("%d\n", str[i]);
+  }
+  
+  return 0;
 }
-
